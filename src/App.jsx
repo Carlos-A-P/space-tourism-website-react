f5d4478ef24a1b60ef2bbc1392d4b64206c025a4
Carlos-A-P <pcarlos491@gmail.com>
2021-11-17 15:54:14 -0800

    added most of Sass structure and now working on porperly importing styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.scss";
+import "./styles/App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Navbar";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 
 function App() {
 	return (
-		<div className="App">
-			<h2>hello world</h2>
-		</div>
+		<Router className="App">
+			<Header />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/Destination" exact component={Destination} />
+				<Route path="/Crew" exact component={Crew} />
+				<Route path="/Technology" exact component={Technology} />
+			</Switch>
+		</Router>
 	);
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/shared/logo.svg";
+import Hamburger_Menu from "./assets/shared/icon-hamburger.svg";
+import Close_Menu from "./assets/shared/icon-close.svg";
+
+export default function navbar() {
+	const [click, setClick] = useState(false);
+	return (
+		<>
+			<header>
+				<div>
+					<img src={logo} alt="space tourism logo" />
+				</div>
+				<button className="menu-icon" aria-controls="primary-navigation">
+					<span class="sr-only" aria-expanded="false">
+						Menu
+					</span>
+				</button>
+				<nav className="navbar">
+					<Link to="">
+						{/* the aria-hidden is so that span won't be read to the screen reader */}
+						<span aria-hidden="true">00</span>Home
+					</Link>
+					<Link to="">
+						<span aria-hidden="true">01</span>Destination
+					</Link>
+					<Link to="">
+						<span aria-hidden="true">02</span>Crew
+					</Link>
+					<Link to="">
+						<span aria-hidden="true">03</span>Technology
+					</Link>
+				</nav>
+			</header>
+		</>
+	);
+}
